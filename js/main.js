2b2c1785120203574c82f59e930da89c0432aa4a
rcpeng36 <rcpeng36@gatech.edu>
2025-12-15 14:16:12 -0700

    adding server

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ d3.select("#chart")
     this.style.backgroundColor = "";
   })
   .text("D3 is working!");
+
+const hello = () => {
+  console.log("hello function");
+};
+
+const circle_op = (id) => {
+  d3.select("#" + id)
+    .attr("fill", "rgba(255, 242, 0, 0.5)")
+    .attr("stroke", "orange")
+    .attr("stroke-width", "5px");
+};
+
+const get_data_from_csv = (source) => {
+  d3.json(`/api/v1/data?source=${encodeURIComponent(source)}`).then((data) => {
+    console.log(data);
+  });
+};
+
+window.hello = hello;
+window.circle_op = circle_op;
+window.get_data_from_csv = get_data_from_csv;
---
+++ server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI,HTTPException
+from fastapi.staticfiles import StaticFiles
+from pyspark.sql import SparkSession
+import pandas as pd
+
+
+spark = SparkSession.builder.appName("6242").getOrCreate()
+
+app = FastAPI()
+
+
+@app.get("/api/v1/data")
+def get_data_csv(source, header=True, inferSchema=True, multiLine=True):
+    try:
+        df = spark.read.csv(path=source, header=header, inferSchema=inferSchema, multiLine=multiLine)
+        pdf = df.toPandas()
+        print(pdf.head())
+        return pdf.to_dict(orient="records")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+app.mount("/", StaticFiles(directory=".",html=True),name="static")
